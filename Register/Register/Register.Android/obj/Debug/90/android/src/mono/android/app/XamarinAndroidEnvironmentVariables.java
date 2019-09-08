4554960c8a7820babbc4afa86c2d554e95fdff7f
Enmanuelolivo <45018826+Enmanuelolivo@users.noreply.github.com>
2019-09-08 11:07:44 -0400

    update

--- Register/Register/Register.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Register/Register/Register.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "ef8f5eed-58ec-439a-a713-08718048c7a0",
+		"XAMARIN_BUILD_ID", "e62af1c3-86e8-4515-b4eb-03bebb2907e2",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
--- Register/Register/Register/ViewModels/RegisterLogViewModel.cs
+++ Register/Register/Register/ViewModels/RegisterLogViewModel.cs
@@ -52,7 +52,7 @@ namespace Register.ViewModels
 
                  else
                  {
-                     await App.Current.MainPage.Navigation.PushModalAsync(new HomePage());
+                     await App.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(new HomePage()));
                  }
              }
             );
